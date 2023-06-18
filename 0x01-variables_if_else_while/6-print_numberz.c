8166021178df41bed94dffbb5a1fe2b0d936ef39
Nozm31 <nomazibuko@gmail.com>
2023-06-18 10:02:05 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ */
+
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return(0);
+}
