0012da9e28ff547c916b50467e5c6aa1dfcb4eea
Nozm31 <nomazibuko@gmail.com>
2023-06-19 09:15:02 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	int n, m, l;
+
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
@@ -16,7 +17,7 @@ int main(void)
 				{
 					putchar(n);
 					putchar(m);
-                                       	putchar(l);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
