2bb8fd7d9de6a5b136d7a5471adfe37faa0da098
Nozm31 <nomazibuko@gmail.com>
2023-06-26 05:11:47 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@
 void puts2(char *str)
 {
 
-        int longi = 0;
+	int longi = 0;
 	int t = 0;
 	char *y = str;
 	int o;
@@ -23,11 +23,11 @@ void puts2(char *str)
 
 	}
 
-        t = longi - 1;
+	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
 
-                if (o % 2 == 0)
+	if (o % 2 == 0)
 
 	{
 
